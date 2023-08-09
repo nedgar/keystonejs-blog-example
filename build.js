0ad9fce57563ec1bf39ef1231cfe4c5829866cc0
Nick Edgar <n.a.edgar@gmail.com>
2023-08-09 13:00:22 -0400

    tweak: add comment to build.js

--- build.js
+++ build.js
@@ -1,3 +1,6 @@
+// This works around a problem with `keystone build` hanging.
+// See https://github.com/keystonejs/keystone/issues/8581#issuecomment-1590581788
+
 const scripts = require("@keystone-6/core/scripts/cli");
 
 (async () => {
